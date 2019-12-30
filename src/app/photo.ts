de32c76498bfc3b8f5c7a05c36c22ff354984860
Damian Smutek <smutek.damian95@gmail.com>
2019-12-30 15:28:39 +0100

    Add photos grid in gallery

--- src/app/photo.ts
+++ src/app/photo.ts
@@ -1,9 +1,12 @@
 export class Photo {
-  constructor(lattitude, longitude) {
+  constructor(lattitude, longitude, src, thumbnailSrc) {
     this.lattitude = lattitude;
     this.longitude = longitude;
+    this.src = src;
+    this.thumbnailSrc = thumbnailSrc;
   }
   lattitude: number;
   longitude: number;
   src : string;
+  thumbnailSrc : string;
 }
